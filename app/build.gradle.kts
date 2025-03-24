279fcd68996d2e276e624405ca4f9cb65e3e1527
caprinixdev <caprinixdev@gmail.com>
2025-03-24 14:50:09 +0700

    update rm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation ("com.github.eriffanani:ContentLoader:1.2.0")
     implementation ("com.facebook.shimmer:shimmer:0.5.0@aar")
     implementation ("androidx.lifecycle:lifecycle-process:2.8.7")
-//    implementation ("com.github.caprinixdev:ultra_a_d:1.0.7")
+//    implementation ("com.github.caprinixdev:ultra_a_d:1.0.8")
 
     implementation (project(":gsadutils"))
 }
